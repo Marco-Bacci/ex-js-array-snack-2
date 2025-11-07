6c6883c2d628d8d17449d5b4e3fd8c89b08bb84c
Marco Bacci <marcobacci94@gmail.com>
2025-11-07 15:27:34 +0100

    Feat: (snack 2) step 1 completed

--- script.js
+++ script.js
@@ -58,3 +58,12 @@ const longBooksTitles = longBooks.map((b) => b.title);
 console.log(longBooksTitles);
 
 longBooksTitles.forEach((b) => console.log(b));
+
+
+// Snack 2 - Il primo libro scontato
+// Creare un array (availableBooks) che contiene tutti i libri disponibili.
+// Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+// Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi)
+
+const availableBooks = books.filter((b)=> b.available = true)
+console.log(availableBooks)
